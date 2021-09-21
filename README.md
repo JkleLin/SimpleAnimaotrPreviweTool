87cb9106fbca8b9c247b3172fb4990e8a541f46c
BigStrongLin <bigstronglin@foxmail.com>
2021-09-21 12:05:00 +0800

    Init Import

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SimpleAnimaotrPreviweTool
+
 An Animator preview tool for Unity
+
+Have you ever wanted to preview the **Animator** in **edit** mode?
+
+Now you can do that.
+
+Import this script to your Editor. Then enjoy it.
+
+**Before active preview, You need to exit animation preview mode**
+
+
+# 简单的动画状态机预览工具
+
+这是一个简单的 Unity Animator 预览工具
+
+你是否想过能在**Editor**模式下预览你通过**Animator**做的混合动画？
+
+现在你可以做到啦！
+
+只需要把该脚本导入到你的Unity工程中，就可以在动画状态机中预览动画了。
+
+**在激活状态机预览前，你需要先关闭Animation的Preview**
+
+![overview](img/OverView.png)
---
+++ Script/AniPreviewTool.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System;
+
+[CustomEditor(typeof(Animator))]
+public class AniPreviewTool : Editor
+{
+    Animator animator;
+
+    private void OnEnable() {
+        if (animator == null) animator = ((Animator)target).GetComponentInChildren<Animator>();
+        EditorApplication.update += Update;
+    }
+
+    private void OnDisable() {
+        if (animator != null) {
+            animator.Rebind();
+            EditorApplication.update -= Update;
+        }
+    }
+
+    private void Update() {
+        if (is_preview && animator != null && animator.enabled) 
+            animator.Update(Time.deltaTime);
+        if (last_preview && !is_preview && animator != null) 
+            animator.Rebind();
+        last_preview = is_preview;
+    }
+
+    bool is_preview = false;
+    bool last_preview = false;
+    Vector2 scroll_rect;
+    public override void OnInspectorGUI() {
+
+        base.OnInspectorGUI();
+
+        if (animator == null) return;
+
+        GUILayout.BeginVertical();
+        is_preview = GUILayout.Toggle(is_preview, "预览动画");
+        if (AnimationMode.InAnimationMode()) {
+            is_preview = false;
+        }
+        if (is_preview) {
+            StopAnimationMode();
+            scroll_rect = GUILayout.BeginScrollView(scroll_rect);
+            foreach (AnimatorControllerParameter parameter in animator.parameters) {
+                GUILayout.BeginHorizontal();
+                switch (parameter.type) {
+                    case AnimatorControllerParameterType.Bool:
+                        GUILayout.Label(parameter.name, GUILayout.Width(105));
+                        GUILayout.FlexibleSpace();
+                        animator.SetBool(parameter.name, GUILayout.Toggle(animator.GetBool(parameter.name), ""));
+                        break;
+                    case AnimatorControllerParameterType.Float:
+                        animator.SetFloat(parameter.name, EditorGUILayout.FloatField(parameter.name, animator.GetFloat(parameter.name)));
+                        break;
+                    case AnimatorControllerParameterType.Int:
+                        animator.SetInteger(parameter.name, EditorGUILayout.IntField(parameter.name, animator.GetInteger(parameter.name)));
+                        break;
+                    case AnimatorControllerParameterType.Trigger:
+                        GUILayout.Label(parameter.name, GUILayout.Width(105));
+                        GUILayout.FlexibleSpace();
+                        if (GUILayout.Toggle(false, "", "Radio")) animator.SetTrigger(parameter.name);
+                        break;
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+        GUILayout.EndVertical();
+
+    }
+
+    void StopAnimationMode() {
+        if (AnimationMode.InAnimationMode())
+            AnimationMode.StopAnimationMode();
+    }
+
+}
